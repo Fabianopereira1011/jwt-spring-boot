7d2a64f8b6640577abc5da1a45d255f78220f595
Fabiano Pereira <fabianopereira1011@gmail.com>
2021-12-27 10:56:29 -0300

    created with spring.io

---
+++ src/test/java/fabianopereira1011/com/github/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package fabianopereira1011.com.github.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
